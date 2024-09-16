6d0ff0c23961bd22c3eae1db9ca4ff512e9019d2
ryanboehm123 <ryan.boehm@mymail.champlain.edu>
2024-09-16 11:03:56 -0400

    Xorshift randomization implemented

--- docs/artificialintelligence/assignments/rng/rng.cpp
+++ docs/artificialintelligence/assignments/rng/rng.cpp
@@ -5,24 +5,21 @@
 const std::string TEST_FOLDER = "\\tests\\";
 unsigned int xorShift(unsigned int seed, int r1, int r2);
 int main(){
-  // code here
-  unsigned int seed, N, min, max;
-  std::cin >> seed >> N >> min >> max;
-  unsigned int i;
-  for(i = N; i >= 1; i--)
-  {
-    //Run xor shift
-    seed = xorShift(seed, min, max);
+  unsigned int seed, timesToRun, lowerBound, upperBound;
+  std::cin >> seed >> timesToRun >> lowerBound >> upperBound;
+
+  for (unsigned int i = timesToRun; i > 0; i--) {
+    seed = xorShift(seed, lowerBound, upperBound);
   }
 }
 //The purpose of this function is to take the number and xor shift it to output a pseudo-random number
     unsigned int xorShift(unsigned int seed, int r1, int r2)
 {
-  seed = seed xor (seed << 13);
-  seed = seed xor (seed >> 17);
-  seed = seed xor (seed << 5);
-  int value = r1 + (seed % (r2 - r1 + 1)); //clamps the value to between r1 and r2
-          //output the new values
-          std::cout << value << std::endl;
+  seed ^= seed << 13;
+  seed ^= seed >> 17;
+  seed ^= seed << 5;
+
+  int value = seed % (r2 - r1 + 1) + r1;
+  std::cout << value << std::endl;
   return seed;
 }
